0710a6b82290b0213a4ee6880003aa69296888b9
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2023-08-26 21:30:00 -0700

    added model classes

--- backend/messages/models.py
+++ backend/messages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Texts(models.Model):
+  text = models.TextField()
+  channel = models.ForeignKey('Channel', on_delete=models.CASCADE)
+  last_modified = models.DateTimeField(auto_now=True)
+  created_on=models.DateTimeField(auto_now_add=True)
+
+class Videos(models.Model):
+  video = models.FilePathField(path="static/videos")
+  channel = models.ForeignKey('Channel', on_delete=models.CASCADE)
+  created_on=models.DateTimeField(auto_now_add=True)
+  
+class Images(models.Model):
+  image = models.FilePathField(path='static/img')
+  channel = models.ForeignKey('Channel', on_delete=models.CASCADE)
+  created_on=models.DateTimeField(auto_now_add=True)
+  
--- backend/servers_channels/models.py
+++ backend/servers_channels/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Servers(models.Model):
+  name = models.CharField(max_length=25)
+  members = models.ManyToManyField('User', related_name='servers')
+  description = models.TextField()
+  logo = models.FilePathField(path='/img')
+  
+class Channels(models.Model):
+  name = models.CharField(max_length=25)
+  server = models.ForeignKey('Servers', on_delete=models.CASCADE)
+  
+  
