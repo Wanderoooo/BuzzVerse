3585049d6cce796277e848b37301b0f9f7908aad
Andrew Wang <andrewzlwang@outlook.com>
2023-09-04 18:08:50 -0700

    whoops

--- frontend/src/components/ChatView.js
+++ frontend/src/components/ChatView.js
@@ -2,17 +2,6 @@ import React from "react";
 import '@radix-ui/themes/styles.css';
 import { Box, Flex } from "@radix-ui/themes";
 
-class Message {
-    constructor(text, time) {
-        this.text = text;
-        this.time = time;
-    }
-
-    get text() {
-        return this.text;
-    }
-}
-
 
 // takes a list of messages sorted by time, with messages[0] as the latest message
 export default function ChatView({messages}) {
