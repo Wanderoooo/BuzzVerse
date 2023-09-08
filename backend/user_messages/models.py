c1b01819aa11786921c604896583c8c8684ff678
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2023-09-08 16:01:28 -0700

    create .gitignore file for venv

--- backend/user_messages/models.py
+++ backend/user_messages/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+# class Message -> 3 subclasses (composite pattern)
+# http://127.0.0.1:8000/servers_channels/servers/1/channels/1/messages/1
+
+
 class Text(models.Model):
   text = models.TextField()
   channel = models.ForeignKey('servers_channels.Channel', on_delete=models.CASCADE)
