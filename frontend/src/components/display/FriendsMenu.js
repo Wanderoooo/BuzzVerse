9fb6e0e7c7bcbbf8f4619f59fa17e8face542da9
Andrew Wang <andrewzlwang@outlook.com>
2023-09-10 14:24:49 -0700

    added option to change between friends messages

--- frontend/src/components/display/FriendsMenu.js
+++ frontend/src/components/display/FriendsMenu.js
@@ -2,13 +2,15 @@ import React from "react";
 import '@radix-ui/themes/styles.css' 
 import { Button, Flex, Text } from "@radix-ui/themes"
 import { Friend } from "@/pages/_app";
+import FriendButton from "../input/FriendButton";
 
 export default function FriendsMenu(props) {
 
     return (
-        <Flex direction='column' gap='6'>
+        <Flex direction='column' gap='3'>
             {props.friendList.map((friend) => 
-                <Button size='3'>{friend.name}</Button>
+                <FriendButton name={props.friendList[props.friendList.indexOf(friend)].name} id={props.friendList.indexOf(friend)} 
+                    onClick={props.clicks[props.friendList.indexOf(friend)]}/>
             )}
         </Flex>
     )
