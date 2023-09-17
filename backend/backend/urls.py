c3007a7aae4c3fcbeebe36664724fa5438316000
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2023-09-17 14:56:23 -0700

    Stanley's auth token sys incorp

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,7 +21,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('servers_channels.urls')),
-     path('', include('user_messages.urls')),
-      path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('', include('user_messages.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
