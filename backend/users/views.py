b8be8bd06f0d25cfc411cff79c40173260a7d8b6
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2023-09-17 14:13:34 -0700

    README.md backend

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
 from django.contrib.auth.models import User
@@ -10,14 +11,18 @@ from .serializers import UserSerializer, UserProfileSerializer
 from rest_framework.permissions import IsAuthenticated
 
 class UserCreateView(APIView):
-    def post(self, request):
+     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
             user.set_password(serializer.validated_data['password'])
             user.save()
             UserProfile.objects.create(user=user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            
+            return Response({"access_token": access_token}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserLoginView(APIView):
@@ -27,11 +32,14 @@ class UserLoginView(APIView):
         try:
             user = User.objects.get(username=username)
             if user.check_password(password):
-                return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+                refresh = RefreshToken.for_user(user)
+                access_token = str(refresh.access_token)
+                
+                return Response({"access_token": access_token}, status=status.HTTP_200_OK)
             return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
         except User.DoesNotExist:
             return Response({"error": "User does not exist"}, status=status.HTTP_400_BAD_REQUEST)
-
+        
 class UserProfileView(APIView):
     permission_classes = [IsAuthenticated]
 
---
+++ frontend/src/pages/demo.js
@@ -0,0 +1,11 @@
+
+
+export default function Demo() {
+  
+
+  return (
+  <div>
+    <h1>backend connectivity demo</h1>
+
+  </div>)
+}
