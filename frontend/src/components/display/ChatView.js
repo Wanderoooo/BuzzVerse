914e8d1eb1b85304cf5b6bd22412524c029c2fff
Andrew Wang <andrewzlwang@outlook.com>
2023-09-16 22:16:01 -0700

    implemented texting in servers

--- frontend/src/components/display/ChatView.js
+++ frontend/src/components/display/ChatView.js
@@ -1,16 +1,22 @@
 import React from "react";
 import '@radix-ui/themes/styles.css';
-import { Text, Flex } from "@radix-ui/themes";
+import { Text, Flex, Separator } from "@radix-ui/themes";
+import { Message } from "@/pages/_app";
 
 
 // takes a list of messages sorted by time, with messages[0] as the latest message
 export default function ChatView(props) {
-
+    const months = ["January","February","March","April","May","June","July",
+    "August","September","October","November","December"];
     return (
         <Flex direction="column" gap="3">
             {props.messages.map((msg) => {
              return (
-                   <Text align='right' key={props.username + ''}>{msg.text}</Text>
+                    <>
+                    <Text align='right' size='1'>{msg.time.getHours() + ':' + msg.time.getMinutes() + ', ' 
+                        + months[msg.time.getMonth()] + ' ' + msg.time.getDate() + ' '}</Text>
+                    <Text align='right' key={props.username + ''}>{msg.text}</Text>
+                    </>
                 )})}
         </Flex>
     )
--- frontend/src/components/display/Dashboard.js
+++ frontend/src/components/display/Dashboard.js
@@ -25,38 +25,49 @@ export default function DashBoard() {
     const testFriend5 = new Friend('stanley', 'stan');
     const testFriend6 = new Friend('brandon', 'kyspire');
     const [friendList, setFriendList] = useState([testFriend, testFriend2, testFriend3, testFriend4, testFriend5 ]);
-    const [selectedFriend, setSelectedFriend] = useState(0);
-
-    let initalMessages = [];
-    for (let i = 0; i < friendList.length; i++) {
-        initalMessages.push([]);
-    }
-    const [messages, setMessages] = useState(initalMessages); //ith element in messages is an array of messages with the ith friend
+    const [selectedChannel, setSelectedChannel] = useState(0);
     
     const [sendContent, setSendContent] = useState('');
 
     function renderServer(server) {
         if (server == 'friends') {
             return (
-                <FriendsMenu friendList={friendList} clicks={onClickFriendFunctions}/>
+                <FriendsMenu friendList={friendList} clicks={onClickChannelFunctions}/>
             )
         } else {
             console.log(selectedServer);
             console.log(serverList[selectedServer]);
             console.log(serverList[selectedServer].channels);
             return (
-                <ServerMenu channelList={serverList[selectedServer].channels} clicks={onClickServerFunctions}/> //fix clicks
+                <ServerMenu channelList={serverList[selectedServer].channels} clicks={onClickChannelFunctions}/> //fix clicks
             )
         }
     }
 
 
-    const testServer1 = new MessagingServer('twitter gang', 'https://cdn-icons-png.flaticon.com/512/124/124021.png', [], ['kys', 'pee']);
-    const testServer2 = new MessagingServer('ig', 'https://cdn-icons-png.flaticon.com/512/2111/2111463.png', [], ['coding', 'nsfw']);
+    const testServer1 = new MessagingServer('twitter gang', 'https://cdn-icons-png.flaticon.com/512/124/124021.png', [], ['x', 'xx', 'no']);
+    const testServer2 = new MessagingServer('ig', 'https://cdn-icons-png.flaticon.com/512/2111/2111463.png', [], ['coding', 'reels']);
     const [selectedServer, setSelectedServer] = useState(0); //0 -> friends, 1 onwards -> servers
     const [serverList, setServerList] = useState(['friends', testServer1, testServer2]); //serverList[0] = friends list, 1 onwards -> actual servers
 
-    const chat = <ChatView messages={messages[selectedFriend]} />;
+
+    let initalFriendMessages = [];
+    for (let i = 0; i < friendList.length; i++) {
+        initalFriendMessages.push([]);
+    }
+    let initalMessages = [];
+    initalMessages.push(initalFriendMessages);
+    let toPush;
+    for (let i = 1; i < serverList.length; i++) {
+        toPush = [];
+        for (let j = 0; j < serverList[i].channels.length; j++) {
+            toPush.push([]);
+        }
+        initalMessages.push(toPush);
+    }
+    const [messages, setMessages] = useState(initalMessages); //ith element in messages is a 2d array of messages with the ith server
+    
+    const chat = <ChatView messages={messages[selectedServer][selectedChannel]} />;
 
     function getSendContent(val) {
         document.addEventListener('keydown', function(event){
@@ -81,41 +92,52 @@ export default function DashBoard() {
             return;
         }
         console.log(text);
-        const res = [];
-        for (let i = 0; i < messages[selectedFriend].length; i++) {
-            res.push(messages[selectedFriend][i]);
+        let res1 = [];
+        let res2 = [];
+        let newMessages = [];
+        
+        for (let i = 0; i < messages.length; i++) {
+            for (let j = 0; j < messages[i].length; j++) {
+                for (let k = 0; k < messages[i][j].length; k++) {
+                    res1.push(messages[i][j][k]);
+                }
+                res2.push(res1);
+                res1 = [];
+            }
+            newMessages.push(res2);
+            res2 = [];
+            res1 = [];
         }
-        res.push(new Message(text, 10, 'andrew'));
 
-        // lines below replace a single entry in messages
+        newMessages[selectedServer][selectedChannel].push(new Message(text, new Date(), 'andrew'))
 
-        const copy = [];
-        for (let i = 0; i < selectedFriend; i++) {
-            copy.push(messages[i]);
-        }
-        copy.push(res);
-        for (let i = selectedFriend + 1; i < messages.length; i++) {
-            copy.push(messages[i])
-        }
-        setMessages(copy);
+        setMessages(newMessages);
         document.getElementById('sendBox').value = "";
         setSendContent(undefined);
     } 
 
-    let onClickFriendFunctions = [];
+    let onClickChannelFunctions = [];
     for (let i = 0; i < friendList.length; i++) {
-        onClickFriendFunctions.push(() => setSelectedFriend(i));
+        onClickChannelFunctions.push(() => setSelectedChannel(i));
     }
     
     let onClickServerFunctions = [];
     for (let i = 0; i < serverList.length; i++) {
         onClickServerFunctions.push(() => setSelectedServer(i));
     }
+
+    function renderHeader(friendSelected, channel) {
+        if (friendSelected) {
+            return <Heading>Chat with {friendList[selectedChannel].name}</Heading>;
+        }
+        return <Heading># {serverList[selectedServer].channels[selectedChannel]}</Heading>
+    }
   
     return (
         
         <Theme appearance="dark" accentColor="teal" grayColor="gray" radius="full">
-            <Text>{selectedServer}</Text>
+            <Text>Selected Server: {selectedServer}  </Text>
+            <Text>Selected Channel: {selectedChannel}</Text>
             <Grid columns="6" style={{gridTemplateColumns:'1fr 0fr 2fr 0fr 8fr 4fr'}} m='4' gap='4'>
                 <Flex direction="column" gap="4" width='100%'>
                     <LeftMenu serverList={serverList} clicks={onClickServerFunctions}/>
@@ -127,7 +149,7 @@ export default function DashBoard() {
                 </Flex>
                 <Separator orientation="vertical" size='4'/>
                 <Flex direction="column" gap="4">
-                    <Heading>Chat with {friendList[selectedFriend].name}</Heading>
+                    {renderHeader(selectedServer === 0, selectedChannel)}
                     <Separator orientation="horizontal" size='4' />
                     <div className='scrollable'>
                         {chat}
