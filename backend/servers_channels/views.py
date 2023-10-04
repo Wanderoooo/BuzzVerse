c642e9a422e1b79cae08fe28dc77662d3cf73a79
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2023-10-04 16:41:28 -0700

    disable auth for development

--- backend/servers_channels/views.py
+++ backend/servers_channels/views.py
@@ -8,7 +8,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 from users.models import UserProfile
 from django.core.exceptions import PermissionDenied
 
