6c9763b9eb3d1572007d2444607306da03e69947
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2023-10-04 16:50:33 -0700

    fix server creation unauth access

--- backend/servers_channels/views.py
+++ backend/servers_channels/views.py
@@ -34,7 +34,7 @@ class ServerViewSet(viewsets.ModelViewSet):
         # Response({"error": "User not authenticated"}, status=status.HTTP_403_FORBIDDEN)
     
 def create(self, request, *args, **kwargs):
-    if request.user.is_authenticated:
+    # if request.user.is_authenticated:
         user_profile = UserProfile.objects.get(user=request.user)
         serializer = ServerSerializer(data=request.data)
         if serializer.is_valid():
@@ -42,8 +42,8 @@ def create(self, request, *args, **kwargs):
             server.user_profiles.add(user_profile)  # Automatically add the user's profile
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    else:
-        return Response({"error": "User not authenticated"}, status=status.HTTP_403_FORBIDDEN)
+    # else:
+    #     return Response({"error": "User not authenticated"}, status=status.HTTP_403_FORBIDDEN)
 
 
 
